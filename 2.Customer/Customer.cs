606928b5abded030175e95f5fa55215b16b19c1f
idinkova <inna.dinkova@gmail.com>
2016-03-24 21:41:33 +0200

    Added files via upload

---
+++ 2.Customer/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Customer
+{
+    public class Customer : ICloneable, IComparable<Customer>
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private long id;
+        private string permanentAddress;
+        private string mobilePhone;
+        private string email;
+
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(Customer other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+//first name, middle name and last name, ID (EGN), permanent address, mobile phone, e-mail, list of payments and customer type. 
---
+++ 2.Customer/Payment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Customer
+{
+    public class Payment
+    {
+        private string productName;
+        private decimal price;
+
+        public Payment(string productName, decimal price)
+        {
+            this.ProductName = productName;
+            this.Price = price;
+        }
+
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
+//product name and price.
